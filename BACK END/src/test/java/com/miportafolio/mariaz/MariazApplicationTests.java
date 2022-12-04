4c49a5db9fa2d018b630e5d533fd7530d2bf1e74
Mareh23 <marehlof@gmail.com>
2022-12-04 17:19:57 -0300

    back end carpeta

---
+++ BACK END/src/test/java/com/miportafolio/mariaz/MariazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportafolio.mariaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
