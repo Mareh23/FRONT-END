de7cbecdd5b9351973394e9421cd7cc87fae05d3
Mareh23 <marehlof@gmail.com>
2023-01-11 23:05:43 -0300

    deploy firebase

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Educacion } from '../model/educacion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/';
+  URL = 'https://back-end-production-1a2c.up.railway.app/educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  UTL: 'https://back-end-production-1a2c.up.railway.app/'
 };
