fca310c0ac6affef8d1abc95d31bb9236297d38b
Mareh23 <marehlof@gmail.com>
2023-01-13 22:00:14 -0300

    proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './components/home/home.component';
 import { EditSkillComponent } from './components/hys/edit-skill.component';
 import { NewSkillComponent } from './components/hys/new-skill.component';
 import { LoginComponent } from './components/login/login.component';
+import { EditProyectoComponent } from './components/proyecto/edit-proyecto.component';
+import { NewProyectoComponent } from './components/proyecto/new-proyecto.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -19,8 +21,9 @@ const routes: Routes = [
   {path: 'editedu/:id', component: EditeducacionComponent},
   {path: 'newskill', component: NewSkillComponent},
   {path: 'editskill/:id', component: EditSkillComponent},
-  {path: 'editacercade/:id', component: EditAcercaDeComponent}
-
+  {path: 'editacercade/:id', component: EditAcercaDeComponent},
+  {path:'nuevoproyect', component: NewProyectoComponent},
+  {path: 'editproyect/:id', component: EditProyectoComponent},
 ];
 
 @NgModule({
