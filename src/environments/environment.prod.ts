59f3110255ca02e1747291d026340173313c336e
Mareh23 <marehlof@gmail.com>
2023-01-14 23:11:39 -0300

    erros

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
     messagingSenderId: '1045740747201',
   },
   production: true,
-  //URL: 'https://back-end-production-1a2c.up.railway.app/'
+  URL: 'https://back-end-production-1a2c.up.railway.app/'
 };
